e2892fa8eec2bea1ce3b815215596ce2dbdc0823
FabianGC0610 <fabian.god06@gmail.com>
2022-01-16 21:21:49 -0600

    Creacion Menu Inferior Home

--- app/src/main/java/mx/kodemia/bookodemia/SiginActivity.kt
+++ app/src/main/java/mx/kodemia/bookodemia/SiginActivity.kt
@@ -1,11 +1,21 @@
 package mx.kodemia.bookodemia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sigin.*
 
 class SiginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sigin)
+
+        textReturnFromSigin.setOnClickListener{
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+
+        bilSigin.setOnClickListener{
+            startActivity(Intent(this,HomeActivity::class.java))
+        }
     }
 }
