d6bf56bdaf637c0008e075ebba66846f71443872
FabianGC0610 <fabian.god06@gmail.com>
2022-01-17 15:38:19 -0600

    Pantalla Home lista

--- app/src/main/java/mx/kodemia/bookodemia/HomeFragment.kt
+++ app/src/main/java/mx/kodemia/bookodemia/HomeFragment.kt
@@ -5,8 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home.*
+import mx.kodemia.bookodemia.adapters.RecyclerViewHome
+import mx.kodemia.bookodemia.model.DataClassHome
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    val listLibros: MutableList<DataClassHome> = mutableListOf()
+    var adapterHome = RecyclerViewHome(listLibros)
+    var parent_view: View? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -14,4 +29,23 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        parent_view = view.findViewById(android.R.id.content)
+        initRecyclerHome()
+    }
+
+    private fun initRecyclerHome(){
+        listLibros.add(DataClassHome("Mil años de muerte","Pancho","Terror"))
+        listLibros.add(DataClassHome("La naranja mecanica","Romero","Erotico"))
+        listLibros.add(DataClassHome("Los hornos de Hitler","Mariano","Relato"))
+        listLibros.add(DataClassHome("Infraestructocho","Alan","Comedia"))
+
+        recyclerView_Home.layoutManager = LinearLayoutManager(activity)
+        recyclerView_Home.setHasFixedSize(true)
+        adapterHome = RecyclerViewHome(listLibros)
+        recyclerView_Home.adapter = adapterHome
+    }
+
 }
---
+++ app/src/main/java/mx/kodemia/bookodemia/adapters/RecyclerViewHome.kt
@@ -0,0 +1,43 @@
+package mx.kodemia.bookodemia.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.snackbar.Snackbar
+import mx.kodemia.bookodemia.R
+import mx.kodemia.bookodemia.model.DataClassHome
+
+class RecyclerViewHome (val home: MutableList<DataClassHome>): RecyclerView.Adapter<RecyclerViewHome.HomeHolder>() {
+
+    class HomeHolder (val view: View) : RecyclerView.ViewHolder(view){
+        val cardView: MaterialCardView = view.findViewById(R.id.cardView_item_home)
+        val textViewTitulo: TextView = view.findViewById(R.id.text_title)
+        val textViewAutor: TextView = view.findViewById(R.id.text_autor)
+        val textViewCategoria: TextView = view.findViewById(R.id.text_categoria)
+
+        fun render(home: DataClassHome){
+            textViewTitulo.setText(home.tituloLibro)
+            textViewAutor.setText(home.autorLibro)
+            textViewCategoria.setText(home.categoriaLibro)
+
+            cardView.setOnClickListener {
+                Snackbar.make(view, "Acceso a ${home.tituloLibro}",Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHome.HomeHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return HomeHolder(layoutInflater.inflate(R.layout.item_cardview_home,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHome.HomeHolder, position: Int) {
+        holder.render(home[position])
+    }
+
+    override fun getItemCount(): Int = home.size
+
+}
